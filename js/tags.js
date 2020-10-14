397ada8aacc04b328489f2712db777311c32fde7
mulili <mulili.nzuki@gmail.com>
2020-10-14 21:05:09 +0300

    fixed rendering bugs

--- js/tags.js
+++ js/tags.js
@@ -1,8 +1,6 @@
 var updateTags = function(){
     history.replaceState({}, "", "/?sort=latest&verified=true");
-    document.querySelector('._1g6qJyApZL1E6iodtNAREf').click();
+    document.querySelector('[title="Marketplace"]').click();
 }
 
-window.addEventListener('load', (event) => {
-    updateTags();
-});
+updateTags();
